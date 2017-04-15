fc1943e9ebdda4918fb99f2d47e44be64b5022dd
kjee99 <kjee99@daum.net>
2017-04-15 17:18:02 +0900

    Add files via upload

---
+++ practice/change a turtle to my face.py
@@ -0,0 +1,18 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 18:41:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import turtle
+>>> turtle.Screen()
+<turtle._Screen object at 0x000001F7F62397F0>
+>>> wn=turtle.Screen()
+>>> t1=turtle.Turtle()
+>>> t1.shape("turtle")
+>>> wn.addshape("D:\과제 사진2.gif")
+>>> t1.shape("D:\과제 사진2.gif")
+>>> t1.fd(100)
+>>> t1.lt(90)
+>>> t1.fd(100)
+>>> t1.lt(90)
+>>> t1.fd(100)
+>>> t1.lt(90)
+>>> t1.fd(100)
+>>> 
